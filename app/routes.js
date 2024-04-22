0fca7ac99eb0803659e483878cbb79f22a88f994
ElmerKainos <123182590+ElmerKainos@users.noreply.github.com>
2024-04-22 10:51:07 +0100

    ss:

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,50 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post ('/has-account', (req,res) => {
+    if (req.session.data.new.hasAccount == "No"){
+  res.redirect ('/can-apply')
+} else {
+     res.redirect ('/email-address')
+   }
+        
+          
+})
+
+router.post ('/can-apply', (req,res) => {
+    if (req.session.data.new.canApply == "No"){
+  res.redirect ('/not-eligible')
+} else {
+     res.redirect ('/email-address')
+   }
+        
+          
+})
+router.post ('/email-address', (req,res) =>  {
+     res.redirect ('/code')
+   }
+        
+          
+)
+
+router.post ('/code', (req,res) =>  {
+
+    res.redirect ('/application')
+  }
+       
+         
+)
+
+router.post ('/code', (req, res) =>  {
+req.session.data.user = {}
+  res.redirect ('/application')
+})
+
+
+
+
+
+
+
+require('./routes/application/show')(router)
---
+++ app/views/routes/application/show.js
@@ -0,0 +1,10 @@
+module.exports = router => { 
+
+router.get ('/application', (req,res)   => {
+
+res.render ('application/show')
+
+
+})
+
+}
